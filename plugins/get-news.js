29c4c870ed382bec1f928725af715d2a09ea77ab
dasundularaka <dasundularaka@gmail.com>
2025-09-05 12:59:47 +0530

    Update get-news.js

--- plugins/get-news.js
+++ plugins/get-news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-> *© Powered By King-Sandesh Md V2 💸*
+> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴢᴇʀᴏ ᴍᴅ 💸*
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
